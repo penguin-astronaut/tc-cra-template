1a8c186275649fdb767eac758f6ebb0eb1dab0a3
Vadim Kasatkin <penguin2804@gmail.com>
2022-07-18 22:10:52 +0400

    --amend

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,17 +4,19 @@ import { Container } from '@layouts/';
 
 import products from '@base/seeders/products';
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export const Home = () => (
   <Container>
     <div className="flex flex-wrap mt-4">
-      {products.map((item) => (
-        <div className="ui-card isAnimated mr-4 mb-4" key={item.id}>
-          <div className="ui-card-body">
+      <Carousel infiniteLoop centerMode showThumbs={false}>
+        {products.map((item) => (
+          <div className="flex items-center justify-center" key={item.id}>
             <img
               src={item.img}
               alt={item.title}
-              style={{ maxWidth: '420px' }}
+              style={{ maxWidth: '320px' }}
             />
             <div className="flex flex-col items-center">
               <span className="ui-title-3">{item.title}</span>
@@ -26,8 +28,8 @@ export const Home = () => (
               </Link>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </Carousel>
     </div>
   </Container>
 );
